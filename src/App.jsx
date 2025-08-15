352e1ab0d0ee7bdc6f92f419980d785cd5f16381
Brian Michel <bmoralescutting@gmail.com>
2025-08-14 21:57:57 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import QuoteBox from "./Components/QuoteBox";
+
+function App() {
+  return (
+   <QuoteBox/>
+  );
+}
+
+export default App;
---
+++ src/data/quotes.js
@@ -0,0 +1,24 @@
+const quotes = [
+  {
+    text: "Lo importante es no dejar de hacerse preguntas.",
+    author: "Albert Einstein"
+  },
+  {
+    text: "Nada en la vida debe ser temido, solo comprendido.",
+    author: "Marie Curie"
+  },
+  {
+    text: "La ciencia no conoce país, porque el conocimiento pertenece a la humanidad y es la antorcha que ilumina el mundo.",
+    author: "Louis Pasteur"
+  },
+  {
+    text: "La ignorancia genera más confianza que el conocimiento.",
+    author: "Charles Darwin"
+  },
+  {
+    text: "La inteligencia es la capacidad de adaptarse al cambio.",
+    author: "Stephen Hawking"
+  }
+];
+
+export default quotes;
