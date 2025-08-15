cd79a412d64e6c6fa5d4e970fc48fea8b62c1097
Brian Michel <bmoralescutting@gmail.com>
2025-08-14 22:02:11 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/random-quotes/', // 👈 importante para GitHub Pages
   plugins: [react()],
 })
